67edda98c08390006d69ecf798242fa4dae51ff7
Deveul <vrenaudi@student.42.fr>
2019-02-01 19:16:44 +0100

    added isnumber in libft

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -6,7 +6,7 @@
 /*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 10:28:24 by smakni            #+#    #+#             */
-/*   Updated: 2018/12/12 11:38:45 by smakni           ###   ########.fr       */
+/*   Updated: 2019/02/01 19:07:10 by vrenaudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -98,6 +98,7 @@ int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_strcmp(const char *s1, const char *s2);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 int					ft_isdigit(int c);
+int					ft_isnumber(char *c);
 int					ft_isascii(int c);
 int					ft_isalpha(int c);
 int					ft_isalnum(int c);
