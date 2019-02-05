35504861a8a8b79ffe0559107d68f2d578548f70
Sabri MAKNI <sabrimakni@gmail.com>
2019-02-05 11:57:06 +0100

    algo more clear

--- src/algo.c
+++ src/algo.c
@@ -6,7 +6,7 @@
 /*   By: smakni <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/04 11:33:26 by smakni            #+#    #+#             */
-/*   Updated: 2019/02/05 11:22:06 by vrenaudi         ###   ########.fr       */
+/*   Updated: 2019/02/05 11:56:22 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,28 +33,20 @@ void	fill_path(t_env *env, int index, int j)
 {
 	int i;
 	int z;
-	int	x;
-	int save;
 
 	i = 0;
-	z = 0;
-	x = 0;
-	save = 0;
 	env->paths[j].connexion = ft_memalloc(100 * sizeof(int));
 	env->paths[j].path = ft_memalloc(100 * sizeof(char *));
 	while (env->nodes[index].end != 1)
 	{
 		ft_printf("NODE = %s\n", env->nodes[index].name);
 		ft_printf(">>>>>>>index = %d<<<<<<<<\n", index);
-	//	env->paths[j].path[i++] = ft_strdup(env->nodes[index].name);
-	//	env->paths[j].len++;
-		z = 0;
 		env->nodes[index].check = 1;
+		z = 0;
 		while (z < env->nodes[index].nb_edges)
 		{
 			ft_printf(">>>edge = %s\n", env->nodes[index].edges[z]);
-			if (env->nodes[env->nodes[index].connexion[z]].check == 0 
-					&& check_path(env, index, z) == 0)
+			if (env->nodes[env->nodes[index].connexion[z]].check == 0)
 			{
 				env->paths[j].path[i] = ft_strdup(env->nodes[index].edges[z]);
 				index = env->nodes[index].connexion[z];
@@ -65,7 +57,6 @@ void	fill_path(t_env *env, int index, int j)
 		env->paths[j].len++;
 		ft_printf("cp = %s\n", env->paths[j].path[i]);
 		i++;
-//		break;
 	}
 }
 
