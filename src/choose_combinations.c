6398e8a9d4c99012af14a2a89a8cb85268bebde5
Deveul <vrenaudi@student.42.fr>
2019-02-20 11:07:41 +0100

    added choose combi

--- src/choose_combinations.c
+++ src/choose_combinations.c
@@ -6,13 +6,46 @@
 /*   By: vrenaudi <vrenaudi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 11:59:09 by vrenaudi          #+#    #+#             */
-/*   Updated: 2019/02/18 13:58:53 by vrenaudi         ###   ########.fr       */
+/*   Updated: 2019/02/20 11:06:33 by vrenaudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <lemin.h>
 
-void		choose_combi(t_env *env)
+static int	sum_lenght(t_env *env, int index)
 {
+	int		i;
+	int		len;
 
+	i = 0;
+	len = 0;
+	while (i < env->final_combi[index].nb_combi)
+	{
+		len += env->paths[env->final_combi[index].index_array[i]].len;
+		i++;
+	}
+	return (len);
+}
+
+void		choose_combinations(t_env *env)
+{
+	int		i;
+	int		nb_line;
+	int		tmp;
+	int		save;
+
+	nb_line = (sum_lenght(env, 0) + env->nb_ants) / env->final_combi[0].nb_combi - 1;
+	save = 0;
+	i = 1;
+	while (i < env->nb_f_c)
+	{
+		tmp = (sum_lenght(env, i) + env->nb_ants) / env->final_combi[i].nb_combi - 1;
+		if (tmp < nb_line)
+		{
+			nb_line = tmp;
+			save = i;
+		}
+		i++;
+	}
+	env->c_c = save;
 }
--- src/move_ants.c
+++ src/move_ants.c
@@ -6,7 +6,7 @@
 /*   By: vrenaudi <vrenaudi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 15:04:12 by vrenaudi          #+#    #+#             */
-/*   Updated: 2019/02/19 15:35:35 by vrenaudi         ###   ########.fr       */
+/*   Updated: 2019/02/20 11:07:10 by vrenaudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void		move_to_last_room(t_env *env, int *tmp, int j)
 {
 	if (env->rooms[tmp[j - 1]].room_content != 0)
 	{
-//		ft_printf("L%d-%s ", env->rooms[tmp[j - 1]].room_content, env->rooms[env->end_index].name);
+		ft_printf("L%d-%s ", env->rooms[tmp[j - 1]].room_content, env->rooms[env->end_index].name);
 		env->rooms[tmp[j - 1]].room_content = 0;
 		env->nb_ants--;
 	}
@@ -24,7 +24,7 @@ void		move_to_last_room(t_env *env, int *tmp, int j)
 
 void		move_in_graph(t_env *env, int *tmp, int j)
 {
-//	ft_printf("L%d-%s ", env->rooms[tmp[j - 1]].room_content, env->rooms[tmp[j]].name);
+	ft_printf("L%d-%s ", env->rooms[tmp[j - 1]].room_content, env->rooms[tmp[j]].name);
 	env->rooms[tmp[j]].room_content = env->rooms[tmp[j - 1]].room_content;
 	env->rooms[tmp[j - 1]].room_content = 0;
 }
@@ -36,7 +36,7 @@ void		move_from_start(t_env *env, int *ants_in, int j, int i)
 	tmp = env->paths[env->final_combi[env->c_c].index_array[i]].path;
 	if (env->paths[env->final_combi[env->c_c].index_array[i]].ants_launched < env->final_combi[env->c_c].ants_by_index[i])
 	{
-//		ft_printf("L%d-%s ", (*ants_in), env->rooms[tmp[1]].name);
+		ft_printf("L%d-%s ", (*ants_in), env->rooms[tmp[1]].name);
 		env->rooms[tmp[j]].room_content = (*ants_in)++;
 		env->paths[env->final_combi[env->c_c].index_array[i]].ants_launched++;
 	}
@@ -58,7 +58,7 @@ void		move_ants(t_env *env, t_ants ants)
 		while (++i < ants.nb_path)
 		{
 			tmp = env->paths[env->final_combi[env->c_c].index_array[i]].path;
-			j = env->paths[env->final_combi[env->c_c].index_array[i]].len + 1;
+			j = env->paths[env->final_combi[env->c_c].index_array[i]].len;
 			move_to_last_room(env, tmp, j);
 			while (--j > 0)
 				if (env->rooms[tmp[j]].room_content == 0)
@@ -70,7 +70,7 @@ void		move_ants(t_env *env, t_ants ants)
 				}
 		}
 		line_printed++;
-//		ft_putendl("");
+		ft_putendl("");
 	}
 	ft_printf("line printed:%d\n", line_printed);
 }
